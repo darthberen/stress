f7add1d7c560f28c3eb75dbfe531fd4492ebb136
matt price <mattprice11@gmail.com>
2016-09-21 11:25:08 -0400

    cleaner impl

--- memory/main.c
+++ memory/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 
 #define USAGE \
 "Usage:\n" \
@@ -10,6 +11,9 @@
 "    seconds: the time interval in seconds that the memory coordinator will \n" \
 "             malloc memory\n" \
 "    size: the size in bytes to be allotted in memory each interval\n"
+
+#define MB (1024 * 1024)
+#define GB (1024 * 1024 * 1024)
 
 int stress(int interval_seconds, unsigned long byte_size);
 
@@ -28,18 +32,28 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    byte_size = atoi(argv[1]);
+    byte_size = strtol(argv[2], NULL, 10);
     if (byte_size <= 0) {
         fprintf(stderr, "Invalid value '%lu' for byte size\n", byte_size);
         return EXIT_FAILURE;
     }
 
+    printf("Writing %lu bytes every %d seconds\n", byte_size, interval_seconds);
     return stress(interval_seconds, byte_size);
 }
 
 int stress(int interval_seconds, unsigned long byte_size) {
+    char * b;
+    unsigned long long total;
+
     while (true) {
-        malloc(byte_size);
+        b = malloc(byte_size);
+        memset(b, 0, byte_size);
+        for (int i = 0; i < byte_size; i++) {
+            b[i] = 'x';
+        }
+        total += byte_size;
+        printf("Total written to memory: %llu (%lluMiB or %lluGiB)\n", total, total/MB, total/GB);
         sleep(interval_seconds);
     }
 
